652ab798c61d5f5436f9201ba4547721032f0f53
liufulin90 <liufulin90@163.com>
2017-02-07 20:13:03 +0900

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
+import {myPlugin} from './plugins'
 Vue.use(Vuex)
+
 const store = new Vuex.Store({
   state: {
     count: 0,
@@ -10,7 +12,23 @@ const store = new Vuex.Store({
     increment (state) {
       state.count++
     }
-  }
+  },
+  actions: {
+    increment (context) {
+      context.commit('increment')
+    }
+  },
+  modules: {
+    // key: {}
+  },
+  getters: {
+    // state,     // 如果在模块中定义则为模块的局部状态
+    // getters,   // 等同于 store.getters
+    // rootState  // 等同于 store.state
+  },
+  // Vuex 的 store 接受 plugins 选项，这个选项暴露出每次 mutation 的钩子。Vuex 插件就是一个函数，它接收 store 作为唯一参数：
+  plugins: [myPlugin],
+  strict: false // 默认值: false使 Vuex store 进入严格模式，在严格模式下，任何 mutation 处理函数以外修改 Vuex state 都会抛出错误。
 })
 
 export default store
